c088bc6ba014b39b73a491a0d08a67e9b1575f91
Ciprian Popescu <getbutterfly@gmail.com>
2017-10-20 10:13:47 +0000

    Add code quality changes

--- footer.php
+++ footer.php
@@ -5,17 +5,17 @@
     <div id="footer-content">
         <div class="footer-widget wide">
             <div class="widget-item">
-                <?php if(is_active_sidebar('footer-widget-1')) dynamic_sidebar('footer-widget-1'); ?>
+                <?php if (is_active_sidebar('footer-widget-1')) dynamic_sidebar('footer-widget-1'); ?>
             </div>
         </div>
         <div class="footer-widget wide">
             <div class="widget-item">
-                <?php if(is_active_sidebar('footer-widget-2')) dynamic_sidebar('footer-widget-2'); ?>
+                <?php if (is_active_sidebar('footer-widget-2')) dynamic_sidebar('footer-widget-2'); ?>
             </div>
         </div>
         <div class="footer-widget wide">
             <div class="widget-item">
-                <?php if(is_active_sidebar('footer-widget-3')) dynamic_sidebar('footer-widget-3'); ?>
+                <?php if (is_active_sidebar('footer-widget-3')) dynamic_sidebar('footer-widget-3'); ?>
             </div>
         </div>
         <div class="clearfix"></div>
@@ -23,7 +23,7 @@
     <div class="attribution">
         <div class="copyright">
 			<?php wp_nav_menu(array('theme_location' => 'footer-menu', 'container' => false)); ?>
-            <br><small><?php _e('Powered by', 'noir-ui'); ?> <a href="https://getbutterfly.com/marketplace/noir-ui-theme-wordpress/" rel="external">Noir UI</a>.</small>
+            <br><small><?php _e('Powered by', 'noir-ui'); ?> <a href="https://getbutterfly.com/downloads/noir-ui/" rel="external">Noir UI</a>.</small>
             <br><small><?php echo get_theme_mod('noir_footer_copyright_text'); ?></small>
         </div>
     </div>
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-<meta http-equiv="cleartype" content="on">
 <meta name="MobileOptimized" content="320">
 <meta name="HandheldFriendly" content="True">
 <meta name="apple-mobile-web-app-capable" content="yes">
@@ -15,21 +14,35 @@
 <body <?php body_class(); ?>>
 
 <div class="ui-base" id="menu">
-    <?php if(is_active_sidebar('responsive-menu-widget-1')) dynamic_sidebar('responsive-menu-widget-1'); ?>
-    <?php wp_nav_menu(array('theme_location' => 'responsive-menu', 'container' => false)); ?>
-    <?php if(is_active_sidebar('responsive-menu-widget-2')) dynamic_sidebar('responsive-menu-widget-2'); ?>
+    <?php
+    if (is_active_sidebar('responsive-menu-widget-1')) {
+        dynamic_sidebar('responsive-menu-widget-1');
+    }
+
+    wp_nav_menu(array(
+        'theme_location' => 'responsive-menu',
+        'container' => false,
+    ));
+
+    if (is_active_sidebar('responsive-menu-widget-2')) {
+        dynamic_sidebar('responsive-menu-widget-2');
+    }
+    ?>
 </div>
 
 <div id="panel">
     <div class="js-slideout-toggle ui-base"><i class="material-icons">menu</i></div>
 
-    <?php // Main menu bar ?>
     <div class="hmenu">
         <nav class="ui-base">
-            <?php wp_nav_menu(array('theme_location' => 'main-menu', 'container' => false)); ?>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+            ));
+            ?>
         </nav>
     </div>
-    <?php // ?>
 
     <div id="wrap">
         <div id="wrapper" class="hfeed">
